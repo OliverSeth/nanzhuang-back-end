3b8d7899eb3a83f420cf0beed1f487ff9dbda85c
OliverSeth <szl980731@126.com>
2020-02-04 14:40:58 +0800

    Create Constant Code

---
+++ src/main/java/com/example/demo/utils/ConstantCode.java
@@ -0,0 +1,12 @@
+package com.example.demo.utils;
+
+/**
+ * Created by Oliver Seth on 2020/2/4 14:15
+ */
+public class ConstantCode {
+    public static final Integer HASH_ITERATION = 3;
+
+    public static final Integer BASEEXCEPTION_CODE = 99;
+
+    public static final Integer UNAUTHORIZED_CODE = 98;
+}
