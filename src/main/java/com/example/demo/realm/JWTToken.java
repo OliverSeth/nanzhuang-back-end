0983cde389d9655091fab37211615eb0108705e9
OliverSeth <szl980731@126.com>
2020-02-13 15:54:58 +0800

    JWT config

--- src/main/java/com/example/demo/realm/JWTToken.java
+++ src/main/java/com/example/demo/realm/JWTToken.java
@@ -2,13 +2,15 @@ package com.example.demo.realm;
 
 import org.apache.shiro.authc.AuthenticationToken;
 
+import java.lang.ref.PhantomReference;
+
 /**
  * Created by Oliver Seth on 2020/2/3 15:36
  */
 public class JWTToken implements AuthenticationToken {
     private String token;
 
-    public JWTToken(String token){
+    JWTToken(String token){
         this.token=token;
     }
 
--- src/main/java/com/example/demo/realm/JWTUtils.java
+++ src/main/java/com/example/demo/realm/JWTUtils.java
@@ -1,12 +1,15 @@
-package com.example.demo.utils;
+package com.example.demo.realm;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Oliver Seth on 2020/2/3 15:24
@@ -24,6 +27,14 @@ public class JWTUtils {
                 .withClaim("username", userName)
                 .withExpiresAt(date)
                 .sign(algorithm);
+    }
+
+    public static boolean verify(String token, String username, String secret) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT jwt = verifier.verify(token);
+        Map<String, Claim> claims=jwt.getClaims();
+        return claims.get("username").asString().equals(username);
     }
 
     public static String getUsername(String token) {
