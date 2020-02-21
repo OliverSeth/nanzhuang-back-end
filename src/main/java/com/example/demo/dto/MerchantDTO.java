9bab69885dfa2f759c510b671dc83187fe1dddbc
OliverSeth <szl980731@126.com>
2020-02-21 17:06:45 +0800

    MerchantDTO

---
+++ src/main/java/com/example/demo/dto/MerchantDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+/**
+ * Created by Oliver Seth on 2020/2/21 17:05
+ */
+public class MerchantDTO {
+    private Integer merchantId;
+    private String ownerName;
+    private String businessCode;
+    private String uniqueCode;
+}
