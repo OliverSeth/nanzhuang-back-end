a37d9a17763c16c08d4687215ba3eaa3f472abd6
OliverSeth <szl980731@126.com>
2020-02-26 17:20:13 +0800

    add record controller

--- src/main/java/com/example/demo/controller/SaleRecordController.java
+++ src/main/java/com/example/demo/controller/SaleRecordController.java
@@ -2,21 +2,26 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.SaleRecord;
 import com.example.demo.service.SaleRecordService;
+import com.example.demo.service.UserService;
 import com.example.demo.utils.ConstantCode;
 import com.example.demo.utils.LogUtils;
 import com.example.demo.utils.Result;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +32,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("sale-record")
 public class SaleRecordController {
+    @Autowired
+    UserService userService;
+
     @Autowired
     SaleRecordService saleRecordService;
 
@@ -100,6 +108,81 @@ public class SaleRecordController {
             return Result.success(map);
         } catch (Exception e) {
             LogUtils.getErrorLog("find sale record by query.", e);
+            return Result.exception(ConstantCode.BASEEXCEPTION_CODE, e.toString());
+        }
+    }
+
+    @PostMapping("add")
+    @RequiresRoles(value = {"admin", "merchant"}, logical = Logical.OR)
+    @ApiOperation("添加销售记录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "merchantName", value = "商户经营者", required = true),
+            @ApiImplicitParam(paramType = "query", name = "businessCode", value = "商企代码", required = true),
+            @ApiImplicitParam(paramType = "query", name = "uniqueCode", value = "唯一编码", required = true),
+            @ApiImplicitParam(paramType = "query", name = "periodYear", value = "期数年", required = true),
+            @ApiImplicitParam(paramType = "query", name = "periodMonth", value = "期数月", required = true),
+            @ApiImplicitParam(paramType = "query", name = "periodDays", value = "期数日", required = true),
+            @ApiImplicitParam(paramType = "query", name = "brand", value = "品牌", required = true),
+            @ApiImplicitParam(paramType = "query", name = "region", value = "区域", required = true),
+            @ApiImplicitParam(paramType = "query", name = "productDaleiCode", value = "产品大类代码", required = true),
+            @ApiImplicitParam(paramType = "query", name = "productDaleiName", value = "产品大类名", required = true),
+            @ApiImplicitParam(paramType = "query", name = "productDaleiSales", value = "大类本旬销售额", required = true, dataType = "double"),
+            @ApiImplicitParam(paramType = "query", name = "productZhongleiCode", value = "产品中类代码", required = true),
+            @ApiImplicitParam(paramType = "query", name = "productZhongleiName", value = "产品中类名称", required = true),
+            @ApiImplicitParam(paramType = "query", name = "productZhongleiSalePrice", value = "中类本旬销售额", required = true, dataType = "double"),
+            @ApiImplicitParam(paramType = "query", name = "productZhongleiSaleNumber", value = "中类本旬销售量", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType = "query", name = "representationNumber", value = "规格代表品货号", required = true),
+            @ApiImplicitParam(paramType = "query", name = "currentPrice", value = "本旬价格", required = true, dataType = "double"),
+            @ApiImplicitParam(paramType = "query", name = "currentSales", value = "本旬销量", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType = "query", name = "A", value = "主材质面料"),
+            @ApiImplicitParam(paramType = "query", name = "B", value = "面料类型"),
+            @ApiImplicitParam(paramType = "query", name = "C", value = "领型"),
+            @ApiImplicitParam(paramType = "query", name = "D", value = "下摆"),
+            @ApiImplicitParam(paramType = "query", name = "E", value = "门襟"),
+            @ApiImplicitParam(paramType = "query", name = "F", value = "款式细节"),
+            @ApiImplicitParam(paramType = "query", name = "note", value = "备注"),
+    })
+    public Result addSaleRecord(HttpServletRequest request) {
+        String username = (String) request.getAttribute("username");
+        String merchantName = request.getParameter("merchantName");
+        String businessCode = request.getParameter("businessCode");
+        String uniqueCode = request.getParameter("uniqueCode");
+        String periodYear = request.getParameter("periodYear");
+        String periodMonth = request.getParameter("periodMonth");
+        String periodDays = request.getParameter("periodDays");
+        String brand = request.getParameter("brand");
+        String region = request.getParameter("region");
+        String productDaleiCode = request.getParameter("productDaleiCode");
+        String productDaleiName = request.getParameter("productDaleiName");
+        double productDaleiSales = Double.parseDouble(request.getParameter("productDaleiSales"));
+        String productZhongleiCode = request.getParameter("productZhongleiCode");
+        String productZhongleiName = request.getParameter("productZhongleiName");
+        double productZhongleiSalePrice = Double.parseDouble(request.getParameter("productZhongleiSalePrice"));
+        Integer productZhongleiSaleNumber = Integer.parseInt(request.getParameter("productZhongleiSaleNumber"));
+        String representationNumber = request.getParameter("representationNumber");
+        double currentPrice = Double.parseDouble(request.getParameter("currentPrice"));
+        Integer currentSales = Integer.parseInt(request.getParameter("currentSales"));
+        String A = request.getParameter("A") == null ? "" : request.getParameter("A");
+        String B = request.getParameter("B") == null ? "" : request.getParameter("B");
+        String C = request.getParameter("C") == null ? "" : request.getParameter("C");
+        String D = request.getParameter("D") == null ? "" : request.getParameter("D");
+        String E = request.getParameter("E") == null ? "" : request.getParameter("E");
+        String F = request.getParameter("F") == null ? "" : request.getParameter("F");
+        String note = request.getParameter("note") == null ? "" : request.getParameter("note");
+        Long recordTimeStamp = System.currentTimeMillis() / 1000;
+        try {
+            if (userService.findByUserName(username).getRoleName().equals("merchant") && !username.equals(merchantName)) {
+                LogUtils.getWarnLog(username, "has no permission to add");
+                return Result.failed(1, "No permission.");
+            }
+            SaleRecord saleRecord = saleRecordService.save(new SaleRecord(username, periodYear, periodMonth, periodDays,
+                    brand, region, merchantName, businessCode, uniqueCode, productDaleiCode, productDaleiName,
+                    productDaleiSales, productZhongleiCode, productZhongleiName, productZhongleiSalePrice,
+                    productZhongleiSaleNumber, representationNumber, currentPrice, currentSales, A, B, C, D, E, F, note,
+                    recordTimeStamp));
+            return Result.success(saleRecord);
+        } catch (Exception e) {
+            LogUtils.getErrorLog(username, "add sale record", e);
             return Result.exception(ConstantCode.BASEEXCEPTION_CODE, e.toString());
         }
     }
