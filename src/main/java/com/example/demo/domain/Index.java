ca9d39abeaddc4f4c15a6f842f359f61e741cfc1
OliverSeth <szl980731@126.com>
2020-03-03 19:33:39 +0800

    table price index

--- src/main/java/com/example/demo/domain/Index.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain;
-
-/**
- * Created by Oliver Seth on 2020/2/22 16:35
- */
-public class Index {
-}
---
+++ src/main/java/com/example/demo/domain/PriceIndex.java
@@ -0,0 +1,130 @@
+package com.example.demo.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by Oliver Seth on 2020/2/22 16:35
+ */
+@Entity
+@Table(name = "price_index")
+public class PriceIndex {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer indexId;
+    private String periodYear;
+    private String periodMonth;
+    private String periodDays;
+    private String code;
+    private String brand;
+    private String region;
+    private String fixedBaseIndex;          //定基指数
+    private String fixedBaseAdjustIndex;
+    private String yearOnYearIndex;         //同比指数
+    private String yearOnYearAdjustIndex;
+    private String chainIndex;              //环比指数
+    private String chainAdjustIndex;
+
+    public Integer getIndexId() {
+        return indexId;
+    }
+
+    public void setIndexId(Integer indexId) {
+        this.indexId = indexId;
+    }
+
+    public String getPeriodYear() {
+        return periodYear;
+    }
+
+    public void setPeriodYear(String periodYear) {
+        this.periodYear = periodYear;
+    }
+
+    public String getPeriodMonth() {
+        return periodMonth;
+    }
+
+    public void setPeriodMonth(String periodMonth) {
+        this.periodMonth = periodMonth;
+    }
+
+    public String getPeriodDays() {
+        return periodDays;
+    }
+
+    public void setPeriodDays(String periodDays) {
+        this.periodDays = periodDays;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getFixedBaseIndex() {
+        return fixedBaseIndex;
+    }
+
+    public void setFixedBaseIndex(String fixedBaseIndex) {
+        this.fixedBaseIndex = fixedBaseIndex;
+    }
+
+    public String getFixedBaseAdjustIndex() {
+        return fixedBaseAdjustIndex;
+    }
+
+    public void setFixedBaseAdjustIndex(String fixedBaseAdjustIndex) {
+        this.fixedBaseAdjustIndex = fixedBaseAdjustIndex;
+    }
+
+    public String getYearOnYearIndex() {
+        return yearOnYearIndex;
+    }
+
+    public void setYearOnYearIndex(String yearOnYearIndex) {
+        this.yearOnYearIndex = yearOnYearIndex;
+    }
+
+    public String getYearOnYearAdjustIndex() {
+        return yearOnYearAdjustIndex;
+    }
+
+    public void setYearOnYearAdjustIndex(String yearOnYearAdjustIndex) {
+        this.yearOnYearAdjustIndex = yearOnYearAdjustIndex;
+    }
+
+    public String getChainIndex() {
+        return chainIndex;
+    }
+
+    public void setChainIndex(String chainIndex) {
+        this.chainIndex = chainIndex;
+    }
+
+    public String getChainAdjustIndex() {
+        return chainAdjustIndex;
+    }
+
+    public void setChainAdjustIndex(String chainAdjustIndex) {
+        this.chainAdjustIndex = chainAdjustIndex;
+    }
+}
