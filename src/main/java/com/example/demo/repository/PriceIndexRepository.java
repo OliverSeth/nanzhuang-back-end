52b1ac9f1ee34e867129dc1921c04d621e4af00e
OliverSeth <szl980731@126.com>
2020-03-04 19:57:36 +0800

    find price index by query sql

---
+++ src/main/java/com/example/demo/repository/PriceIndexRepository.java
@@ -0,0 +1,23 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.PriceIndex;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * Created by Oliver Seth on 2020/3/4 19:49
+ */
+public interface PriceIndexRepository extends JpaRepository<PriceIndex, Integer> {
+    @Query(value = "select p from PriceIndex p " +
+            "where (p.code = :code or :code is null ) " +
+            "and (p.brand = :brand or :brand is null ) " +
+            "and (p.region = :region or :region is null )")
+    List<PriceIndex> findPriceIndexByQuerys(@Param("code") String code,
+                                            @Param("brand") String brand,
+                                            @Param("region") String region,
+                                            Pageable pageable);
+}
