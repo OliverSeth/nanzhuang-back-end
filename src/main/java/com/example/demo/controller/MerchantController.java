7f0dcee3cf664537970ad6567ea4919f4e518e17
OliverSeth <szl980731@126.com>
2020-03-26 22:16:38 +0800

    update

--- src/main/java/com/example/demo/controller/MerchantController.java
+++ src/main/java/com/example/demo/controller/MerchantController.java
@@ -89,7 +89,7 @@ public class MerchantController {
             Merchant merchant = merchantService.findByUsername(username);
             if (merchant == null) {
                 LogUtils.getWarnLog(username, "merchant not exist");
-                return Result.failed(1, "Merchant not exist.");
+                return Result.success(new Merchant());
             }
             LogUtils.getInfoLog(username, "get information for merchant " + merchant.getOwnerName());
             return Result.success(merchant);
